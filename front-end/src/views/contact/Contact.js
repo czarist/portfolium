1f2cfa2abed1b0ce124092bf5eed7684fdd282cf
Cezar  Trentin <czartrentin@gmail.com>
2021-07-02 15:04:47 -0300

    -

--- front-end/src/views/contact/Contact.js
+++ front-end/src/views/contact/Contact.js
@@ -1,40 +1,104 @@
-import React from "react";
-import { CCard, CCardBody, CCardGroup, CCardHeader } from "@coreui/react";
+import React, { Profiler } from "react";
 
-const Contact = () => {
-  return (
-    <CCardGroup columns className="cols-2">
-      <CCard>
-        <CCardHeader>Bar Chart</CCardHeader>
-        <CCardBody></CCardBody>
-      </CCard>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
 
-      <CCard>
-        <CCardHeader>Doughnut Chart</CCardHeader>
-        <CCardBody></CCardBody>
-      </CCard>
+    fetch("http://localhost:3002/send", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === "success") {
+          alert("Message Sent.");
+          this.resetForm();
+        } else if (response.status === "fail") {
+          alert("Message failed to send.");
+        }
+      });
+  }
 
-      <CCard>
-        <CCardHeader>Line Chart</CCardHeader>
-        <CCardBody></CCardBody>
-      </CCard>
+  resetForm() {
+    this.setState({ name: "", email: "", message: "" });
+  }
 
-      <CCard>
-        <CCardHeader>Pie Chart</CCardHeader>
-        <CCardBody></CCardBody>
-      </CCard>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-12 text-center text-white">
+          <h1>CONTACT</h1>
+          <div className="justify-content-center d-flex align-items-center">
+            <p className="w-75 h4">
+              Interested in hiring me for your project or just want to say hi?
+              You can fill in the contact form below or send me an email to{" "}
+              <a href="#" className="blue-link">
+                czartrentin@gmail.com
+              </a>{" "}
+              Want to get connected? Follow me on the social channels below.
+            </p>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="col-12 mt-5">
+          <form
+            id="contact-form"
+            onSubmit={this.handleSubmit.bind(this)}
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" rows="5"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
 
-      <CCard>
-        <CCardHeader>Polar Area Chart</CCardHeader>
-        <CCardBody></CCardBody>
-      </CCard>
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
 
-      <CCard>
-        <CCardHeader>Radar Chart</CCardHeader>
-        <CCardBody></CCardBody>
-      </CCard>
-    </CCardGroup>
-  );
-};
+  onMessageChange(event) {
+    this.setState({ message: event.target.value });
+  }
+
+  handleSubmit(event) {}
+}
 
 export default Contact;
